b85851480a7cd718ef9c2e456d5094d24f21f550
claudiosy <43971712+claudiosy@users.noreply.github.com>
2018-10-24 15:42:39 -0300

    Add files via upload

---
+++ Chama_pContasPessoa.sql
@@ -0,0 +1,49 @@
+set statistics io on
+
+exec pContasPessoa
+	--@TipoPessoa = 'PF',
+	--@Documento = 10474123570,
+	@Nome = 'Beth moraes',
+	--@Email = 'beth_dudu',
+	--@Agencia = '4856',                                                                                                                                                                                                          
+	--@Conta = '209920334',
+	--@Telefone = 11997083079,
+	--@DataInicialAbertura = '2018-10-01 00:00:00',
+	--@DataFinalAbertura = '2018-10-31 23:59:59',
+	--@DataInicialPrimeiraCarga = '2018-10-01 00:00:00',
+	--@DataFinalPrimeiraCarga = '2018-10-31 23:59:59',
+	--@Estado = 'SP',
+	--@Cidade = 'Mogi das Cruzes',
+	--@StatusDocumento = '0',
+	--@Validacao = 'VALIDADO'
+	--@StatusConta = 'A',
+	--@Canal = '55',
+	--@StatusContaId = 1,
+	--@PEP = 1,
+	--@Santander = 1,
+	--@Fatca = 0
+
+
+
+exec pContasPessoa_Old
+	--@TipoPessoa = 'PF',
+	--@Documento = 10474123570,
+	@Nome = 'Beth moraes',
+	--@Email = 'beth_dudu',
+	--@Agencia = '4856',                                                                                                                                                                                                          
+	--@Conta = '209920334',
+	--@Telefone = 11997083079,
+	--@DataInicialAbertura = '2018-10-01 00:00:00',
+	--@DataFinalAbertura = '2018-10-31 23:59:59',
+	--@DataInicialPrimeiraCarga = '2018-10-01 00:00:00',
+	--@DataFinalPrimeiraCarga = '2018-10-31 23:59:59',
+	--@Estado = 'SP',
+	--@Cidade = 'Mogi das Cruzes',
+	--@StatusDocumento = '0',
+	--@Validacao = 'VALIDADO'
+	--@StatusConta = 'A',
+	--@Canal = '55',
+	--@StatusContaId = 1,
+	--@PEP = 1,
+	--@Santander = 1,
+	--@Fatca = 0
